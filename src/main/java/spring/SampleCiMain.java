7045710047e23a1ddb73bbc93117574465957c34
김명구 <myeonggu.kim@kakao.com>
2025-10-13 11:43:58 +0900

    [feature] first commit

---
+++ src/main/java/spring/SampleCiMain.java
@@ -0,0 +1,24 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SampleCiMain {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(SampleCiMain.class, args);
+  }
+  
+}
+
+@RestController
+class PingPongController {
+  
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+}
